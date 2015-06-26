9186fd529fc1e5ffb52ef9afcbf5ef68a9013d9b
GordeyChernyy <gordey@Gordeys-MacBook-Pro-3.local>
2015-06-25 20:21:42 -0400

    aa

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ var buttonNext;
 // -------------- Chapters
 var text6p2;
 // -------------- Path
-var url = "http://gordeychernyy.github.io/motionComics";
+var url = "http://gordeychernyy.github.io/motionComics/broke";
 // -------------- Sprites
 var mahavitra;
 var tree3, tree1, tree2;
